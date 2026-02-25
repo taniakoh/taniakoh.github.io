038f0fb768529899a9c96d8d85f5a1fff82ad9fc
Tania Koh Tze Ern <taniakohtzeern@gmail.com>
2026-02-26 02:18:42 +0800

    fixed description

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Vite + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "tania! | Developer in Singapore",
+  description: "i like building things!",
   navItems: [
     {
       label: "about",
